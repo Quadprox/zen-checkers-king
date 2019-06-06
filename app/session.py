946f1fd6dfe1e66a1897bf2bf035612a4e475153
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-06 20:46:40 +0800

    Updated Shell
    - Player can now select/deselect checkers;
    - Game window displays highlighted move/attack tiles for selected checker;
    - Minor code optimization for app/checker/behavior.

--- app/session.py
+++ app/session.py
@@ -1,5 +1,5 @@
 
 
 
-RENDER_MODE_GLOBAL = 0              # 0 = OpenGL, 1 = Textured
-RENDER_MODE_CHECKER = 1             # 0 = Simplified, 1 = Complex
+RENDER_MODE_GLOBAL = 1              # 0 = OpenGL, 1 = Textured
+RENDER_MODE_CHECKER = 0             # 0 = Simplified, 1 = Complex
