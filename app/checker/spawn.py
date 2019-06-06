a75a070c45ec384119790fefc474e3a36fe40f78
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-06 22:42:36 +0800

    Refractored checker/spawn and checker/mono
    - Checker can no longer self-promote;
    - Checker can no longer spawn 'empty' checker object.

--- app/checker/spawn.py
+++ app/checker/spawn.py
@@ -1,12 +1,4 @@
-import app.board.test as test
 import app.checker.mono as mono
-
-
-def __checker_empty():
-    new_checker = mono.Checker(init_position=[],
-                               init_color='',
-                               init_type_queen=False)
-    return new_checker
 
 
 def __checker_predefined(spawn_position: list, spawn_color: str, spawn_queen: bool = False):
@@ -25,10 +17,7 @@ def __checker_undefined(spawn_position: list):
 
 
 def checker(spawn_position: list, spawn_color: str, spawn_queen: bool = False):
-    if test.position_can_spawn_checker(spawn_position):
-        if spawn_color in ('White', 'Black'):
-            return __checker_predefined(spawn_position, spawn_color, spawn_queen)
-        else:
-            return __checker_undefined(spawn_position)
+    if spawn_color in ('White', 'Black'):
+        return __checker_predefined(spawn_position, spawn_color, spawn_queen)
     else:
-        return __checker_empty()
+        return __checker_undefined(spawn_position)
