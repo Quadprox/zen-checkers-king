5092e0d58a5c6dc3edd15437ee05fe51fdb0b9a3
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-16 23:06:01 +0800

    Updated stopwatch tool
    Stopwatch tool now functions properly based on clock tool and datetime library

--- tools/clock/operate.py
+++ tools/clock/operate.py
@@ -10,11 +10,7 @@ def __is_type_integer(value):
 
 
 def list_adjust(adj_list: list, mod: int, number: int):
-    if __is_type_list(adj_list):
-        if __is_type_integer(mod) and __is_type_integer(number):
-            conv_seconds = convert.list_to_seconds(adj_list)
-            adj_seconds = conv_seconds + int(mod) * int(number)
-            adjusted_list = convert.seconds_to_list(adj_seconds)
-            return adjusted_list
-
-
+    conv_seconds = convert.list_to_seconds(adj_list)
+    adj_seconds = conv_seconds + int(mod) * int(number)
+    adjusted_list = convert.seconds_to_list(adj_seconds)
+    return adjusted_list
---
+++ tools/clock/stopwatch.py
@@ -0,0 +1,53 @@
+from tools.clock import mono as clock, difference, convert, operate
+
+
+class Stopwatch:
+
+    def __init__(self):
+        self.running = False
+
+        # Protected attributes to store and reset session / game values
+        self.__started = None
+        self.__paused = None
+        self.__resumed = None
+        self.__skip = 0
+        self.__stopped = None
+
+    def start(self):
+        self.running = True
+        self.__started = clock.now(formatted=False)
+
+    def pause(self):
+        self.__paused = clock.now(formatted=False)
+
+    def unpause(self):
+        self.__resumed = clock.now(formatted=False)
+        seconds_paused = difference.dt(dt_start=self.__paused,
+                                       dt_end=self.__resumed)
+        self.__skip += seconds_paused
+        self.__paused = None
+        self.__resumed = None
+
+    def stop(self):
+        self.__stopped = clock.now(formatted=False)
+        self.running = False
+
+    def reset(self):
+        if self.running:
+            self.stop()
+        self.__started = None
+        self.__paused = None
+        self.__resumed = None
+        self.__skip = 0
+        self.__stopped = None
+
+    def elapsed(self):
+        if self.__started is not None:
+            start_time = self.__started
+            end_time = self.__stopped
+            if self.running:
+                end_time = clock.now(formatted=False)
+            unskipped_time = difference.dt(dt_start=start_time, dt_end=end_time)
+            final_time = unskipped_time - self.__skip
+            final_time_formatted = convert.seconds_to_list(final_time)
+            return final_time_formatted
