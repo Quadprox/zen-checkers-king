dbcf436978001b217a20c2b1cdae08d1e0d94e72
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-18 00:46:30 +0800

    Updated settings and session
    - Session now holds DEBUG_MODE changable value;
    - Added UI panel surface settings and Button surface settings to the settings list;
    - Added debug mode attribute holder to Shell.

--- app/session.py
+++ app/session.py
@@ -1,6 +1,7 @@
 
 DEV_MODE = False
 TEST_MODE = False
+DEBUG_MODE = False
 
 RENDER_MODE_GLOBAL = 1              # 0 = OpenGL, 1 = Textured
 RENDER_MODE_CHECKER = 1             # 0 = Simplified, 1 = Complex (ignored if RENDER_MODE_GLOBAL == 1)
