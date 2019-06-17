7e9056a58e9829fd933d89fe939b60c2bc80083c
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-18 00:49:02 +0800

    Updated pendil/stamp tool
    Pencil/draw tool can now stamp can now "stamp" small and large buttons filled and empty.

--- tools/pencil/stamp.py
+++ tools/pencil/stamp.py
@@ -21,7 +21,7 @@ def board_highlighted_tile(tile_coord_x: int, tile_coord_y: int, tile_highlight_
         sq_side_len=settings.TILE_HIGHLIGHT_SIDE_LEN,
         sq_border_width=settings.TILE_HIGHLIGHT_BORDER_SIDE_LEN,
         sq_border_color=settings.TILE_HIGHLIGHT_COLOR_MOVE if tile_highlight_type.lower() == 'move' else
-                        settings.TILE_HIGHLIGHT_COLOR_KILL if tile_highlight_type.lower() == 'kill' else
+                        settings.TILE_HIGHLIGHT_COLOR_ATTACK if tile_highlight_type.lower() == 'kill' else
                         settings.ERROR_COLOR_ALT,
         sq_tilt=False
     )
@@ -44,7 +44,7 @@ def board_tile_highlight_attack(tile_coord_x: int, tile_coord_y: int):
         sq_coord_y=tile_coord_y,
         sq_side_len=settings.TILE_HIGHLIGHT_SIDE_LEN,
         sq_border_width=settings.TILE_HIGHLIGHT_BORDER_SIDE_LEN,
-        sq_border_color=settings.TILE_HIGHLIGHT_COLOR_KILL,
+        sq_border_color=settings.TILE_HIGHLIGHT_COLOR_ATTACK,
         sq_tilt=False
     )
 
@@ -159,3 +159,78 @@ def checker(ch_coord_x: int, ch_coord_y: int, ch_color: str, ch_queen: bool = Fa
         1: lambda: __checker_complex(ch_coord_x, ch_coord_y, ch_color, ch_queen)
     }
     render_checker[style]()
+
+
+def __button_large(b_coord_x: int, b_coord_y: int, b_caption: str, b_filled: bool = False):
+    if b_filled:
+        draw.rectangle_solid(
+            rect_coord_x=b_coord_x,
+            rect_coord_y=b_coord_y,
+            rect_width=settings.BUTTON_LARGE_WIDTH,
+            rect_height=settings.BUTTON_LARGE_HEIGHT,
+            rect_color=settings.BUTTON_FILLED_COLOR,
+            rect_tilt=False
+        )
+    draw.rectangle_outline(
+        rect_coord_x=b_coord_x,
+        rect_coord_y=b_coord_y,
+        rect_width=settings.BUTTON_LARGE_WIDTH,
+        rect_height=settings.BUTTON_LARGE_HEIGHT,
+        rect_border_color=settings.BUTTON_BORDER_COLOR,
+        rect_border_width=settings.BUTTON_BORDER_LEN,
+        rect_tilt=False
+    )
+    draw.character(
+        char_string=b_caption,
+        char_coord_x=b_coord_x,
+        char_coord_y=b_coord_y,
+        char_color=settings.BUTTON_CAPTION_COLOR,
+        char_font_size=settings.BUTTON_CAPTION_FONT_SIZE,
+        char_font_name=settings.BUTTON_CAPTION_FONT_NAME,
+        char_rotation_angle=12 if b_filled else 0,
+        char_font_bold=settings.BUTTON_CAPTION_STYLE_BOLD,
+        char_font_italic=settings.BUTTON_CAPTION_STYLE_ITALIC,
+        char_anchor=True
+    )
+
+
+def __button_small(b_coord_x: int, b_coord_y: int, b_caption: str, b_filled: bool = False):
+    if b_filled:
+        draw.rectangle_solid(
+            rect_coord_x=b_coord_x,
+            rect_coord_y=b_coord_y,
+            rect_width=settings.BUTTON_SMALL_WIDTH,
+            rect_height=settings.BUTTON_SMALL_HEIGHT,
+            rect_color=settings.BUTTON_FILLED_COLOR,
+            rect_tilt=False
+        )
+    draw.rectangle_outline(
+        rect_coord_x=b_coord_x,
+        rect_coord_y=b_coord_y,
+        rect_width=settings.BUTTON_SMALL_WIDTH,
+        rect_height=settings.BUTTON_SMALL_HEIGHT,
+        rect_border_color=settings.BUTTON_BORDER_COLOR,
+        rect_border_width=settings.BUTTON_BORDER_LEN,
+        rect_tilt=False
+    )
+    draw.character(
+        char_string=b_caption,
+        char_coord_x=b_coord_x,
+        char_coord_y=b_coord_y,
+        char_color=settings.BUTTON_CAPTION_COLOR,
+        char_font_size=settings.BUTTON_CAPTION_FONT_SIZE,
+        char_font_name=settings.BUTTON_CAPTION_FONT_NAME,
+        char_rotation_angle=12 if b_filled else 0,
+        char_font_bold=settings.BUTTON_CAPTION_STYLE_BOLD,
+        char_font_italic=settings.BUTTON_CAPTION_STYLE_ITALIC,
+        char_anchor=True
+    )
+
+
+def button(b_coord_x: int, b_coord_y: int, b_size: str, b_caption: str, b_filled: bool = False):
+    size = b_size.lower()
+    render_button = {
+        'large': lambda: __button_large(b_coord_x, b_coord_y, b_caption, b_filled),
+        'small': lambda: __button_small(b_coord_x, b_coord_y, b_caption, b_filled),
+    }
+    render_button[size]()
