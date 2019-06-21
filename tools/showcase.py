fac8bba56cea7af0346be24e72f1a1fd7ec31b80
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-21 13:15:04 +0800

    Updated showcase script
    Showcase script can now render a separate UI panel and/or render full project with panel included.

--- tools/showcase.py
+++ tools/showcase.py
@@ -1,5 +1,9 @@
 import arcade
 import app.settings as settings
+import tools.pencil.stamp as stamp
+import random
+import app.board.mono as board
+import app.ui.mono as ui
 
 
 CHECKER_SHOWCASE_POS_X = 64
@@ -9,10 +13,10 @@ CHECKER_SHOWCASE_POS_Y = 64
 def __initialize(showcase_object: str):
     arcade.open_window(
         width=settings.BOARD_SIDE_LEN if showcase_object == 'board' else
-              settings.GAME_WINDOW_WIDTH if showcase_object == 'project' else
+              settings.GAME_WINDOW_WIDTH if showcase_object in ('project', 'panel') else
               CHECKER_SHOWCASE_POS_X * 2 if showcase_object == 'checker' else 0,
         height=settings.BOARD_SIDE_LEN if showcase_object == 'board' else
-               settings.GAME_WINDOW_HEIGHT if showcase_object == 'project' else
+               settings.GAME_WINDOW_HEIGHT if showcase_object in ('project', 'panel') else
                CHECKER_SHOWCASE_POS_Y * 2 if showcase_object == 'checker' else 0,
         window_title=f'{settings.PROJECT_NAME} ({showcase_object} showcase demo)',
         resizable=False,
@@ -31,20 +35,15 @@ def __finish_render():
 
 
 def board_surface(filled: bool = False):
-    from app.board import mono as board
-
     __initialize(showcase_object='board')
     __start_render()
     if filled:
         board.Board().fill()
-    board.Board().display(render_checkers=True)
+    board.Board().display()
     __finish_render()
 
 
 def checker_surface():
-    import tools.pencil.stamp as stamp
-    import random
-
     __initialize(showcase_object='checker')
     __start_render()
     stamp.checker(
@@ -56,4 +55,29 @@ def checker_surface():
     __finish_render()
 
 
-board_surface(filled=True)
+def panel_surface():
+    __initialize(showcase_object='panel')
+    __start_render()
+    panel = ui.UI()
+    panel.set_mode(mode=1)
+    panel.initialize()
+    panel.display()
+    __finish_render()
+
+
+def project_surface():
+    __initialize(showcase_object='project')
+    __start_render()
+
+    # Setting up board (filled with checkers at game start)
+    board.Board().fill()
+    board.Board().display()
+
+    # Setting up UI panel (active game mode)
+    panel = ui.UI()
+    panel.set_mode(mode=1)
+    panel.initialize()
+    panel.display()
+
+
+panel_surface()
