66407f83ea3d899afd93451999bc458274790e50
Andrey Vostokov <41553031+Quadprox@users.noreply.github.com>
2019-06-22 13:20:33 +0800

    Added button ID stamps

--- app/ui/elements/button.py
+++ app/ui/elements/button.py
@@ -1,8 +1,12 @@
 from tools.pencil import stamp
 from app.ui import mapping
+from app import settings
 
 
 class Button:
+
+    ID = None
+
     def __init__(self,init_position: list, init_filled: bool = False):
         self.__caption = None
         self.__size = None
@@ -54,48 +58,70 @@ class Button:
 
 
 class StartButton(Button):
+    ID = settings.BUTTON_ID_START_GAME
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Start')
 
 
 class SettingsButton(Button):
+    ID = settings.BUTTON_ID_SETTINGS
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Settings')
 
 
 class QuitButton(Button):
+    ID = settings.BUTTON_ID_QUIT
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Quit')
 
 
 class PauseButton(Button):
+    ID = settings.BUTTON_ID_PAUSE
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Pause')
 
 
 class RestartButton(Button):
+    ID = settings.BUTTON_ID_RESTART_GAME
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Restart')
 
 
 class EndCurrentButton(Button):
+    ID = settings.BUTTON_ID_END_CONFIRM
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('End game')
 
 
+class StartNewButton(Button):
+    ID = settings.BUTTON_ID_END_CONFIRM
+    def __init__(self, init_position: list, init_filled: bool = False):
+        super().__init__(init_position, init_filled)
+        self.set_caption('Start new')
+
+
 class HintButton(Button):
+    ID = settings.BUTTON_ID_HINT
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Hint')
 
 
 class UndoButton(Button):
+    ID = settings.BUTTON_ID_UNDO
     def __init__(self, init_position: list, init_filled: bool = False):
         super().__init__(init_position, init_filled)
         self.set_caption('Undo')
+
+
+class BackButton(Button):
+    ID = settings.BUTTON_ID_BACK
+    def __init__(self, init_position: list, init_filled: bool = False):
+        super().__init__(init_position, init_filled)
+        self.set_caption('Back')
